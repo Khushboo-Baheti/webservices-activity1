2861df0a5b361eee101abe1449b3d45baac32806
Khushboo Mantri <khushboo@Khushboos-MacBook-Pro.local>
2018-04-18 15:35:16 -0700

    adding project files

---
+++ Webact7/Webact7/Default.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Webact7._Default" %>
+
+<!DOCTYPE html>
+<html>
+<body>
+<form id="form1" runat="server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server">
+    </asp:ScriptManager>
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server" OnLoad="UpdatePanel1_Load ">
+            <ContentTemplate>
+                <fieldset>
+                    <legend>Update Day & Text</legend>
+                    <asp:Timer ID="Timer1" runat="server" Interval="500" OnTick="Timer1_Tick">
+                    </asp:Timer>
+                    <asp:Label ID="year1" runat="server" Text="year"></asp:Label>
+                    </br>
+                    <asp:Label ID="text1" runat="server" Text="text"></asp:Label>
+                </fieldset>
+            </ContentTemplate>
+        </asp:UpdatePanel>
+    <p>
+            <strong><span class="auto-style1">Math Trivia ...</span></strong></p>
+        <p>
+            Question:
+            <asp:Label ID="trivia_question" runat="server" ></asp:Label>
+        </p>
+        <p>
+            What is your answer:
+            <asp:TextBox ID="trivia_answer" runat="server"></asp:TextBox>
+            <asp:Button ID="trivia_submit" runat="server" OnClick="trivia_submit_Click" Text="Check your answer!" />
+        </p>
+        <p>
+            <asp:Label ID="trivia_checkanswer" runat="server"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="trivia_canswer" runat="server" Visible="False" ></asp:Label>
+        </p>
+        <p> ______________________________________________________________________________________________________________</p>
+
+            <p><strong><span class="auto-style1">Sentiment Analysis ...</span></strong></p>
+        <p>
+            Enter your text string:
+            <asp:TextBox ID="sentiment_text" runat="server"></asp:TextBox>
+            <asp:Button ID="sentiment_submit" runat="server" OnClick="sentiment_submit_Click" Text="Analyze text!" />
+        </p>
+        <p>
+            <asp:Label ID="sentiment_answer" runat="server"></asp:Label>
+        </p>
+        <p>
+            ________________________________________________________________________________________________________________________________________</p>
+        <p>
+            <strong><span class="auto-style1">Currency Converter ... Please enter the Capital letters only.</span></strong></p>
+        <p>
+            From:
+            rom" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; To:
+            <asp:TextBox ID="convert_to" runat="server"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="convert_submit" runat="server" OnClick="convert_submit_Click" Text="convert" />
+        </p>
+        <p>
+            <asp:Label ID="convert_rate" runat="server"></asp:Label>
+        </p>
+
+</form>
+    </body>
+    </html>
---
+++ Webact7/Webact7/Default.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Webact7
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack == false)
+            {
+                mathTrivia();
+            }
+        }
+        protected void UpdatePanel1_Load(object sender, EventArgs e)
+        {
+            thisday();
+        }
+        protected void Timer1_Tick(object sender, EventArgs e)
+        {
+            thisday();
+        }
+        void thisday()
+        {
+            try
+            {
+                HttpWebRequest serviceRequest = (HttpWebRequest)WebRequest.Create("https://numbersapi.p.mashape.com/3/1/date?fragment=true&json=true");
+                serviceRequest.Method = "GET";
+                serviceRequest.ContentLength = 0;
+                serviceRequest.ContentType = "text/plain; charset=utf-8";
+                serviceRequest.Accept = "text/plain";
+                serviceRequest.Headers.Add("X-Mashape-Key", "WpWNTQDtlSmshAjiVtLymV1curwkp12GEdTjsn6XcyIMBQT6g6");
+                HttpWebResponse serviceResponse = (HttpWebResponse)serviceRequest.GetResponse();
+                Stream receiveStream = serviceResponse.GetResponseStream();
+                Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+                StreamReader readStream = new StreamReader(receiveStream, encode, true);
+                string responseFromServer = readStream.ReadToEnd();
+                JavaScriptSerializer js = new JavaScriptSerializer();
+                var obj = js.Deserialize<dynamic>(responseFromServer);
+                year1.Text = obj["year"].ToString();
+                text1.Text = obj["text"];
+            }
+            catch(Exception)
+            {
+                year1.Text = "reload the page again";
+            }
+        }
+        void mathTrivia()
+        {
+            HttpWebRequest serviceRequest = (HttpWebRequest)WebRequest.Create("https://numbersapi.p.mashape.com/1234/trivia?fragment=true&json=true&max=20&min=10");
+            serviceRequest.Method = "GET";
+            serviceRequest.ContentLength = 0;
+            serviceRequest.ContentType = "text/plain; charset=utf-8";
+            serviceRequest.Accept = "text/plain";
+            serviceRequest.Headers.Add("X-Mashape-Key", "WpWNTQDtlSmshAjiVtLymV1curwkp12GEdTjsn6XcyIMBQT6g6");
+            HttpWebResponse serviceResponse = (HttpWebResponse)serviceRequest.GetResponse();
+            Stream receiveStream = serviceResponse.GetResponseStream();
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader readStream = new StreamReader(receiveStream, encode, true);
+            string responseFromServer = readStream.ReadToEnd();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            var obj = js.Deserialize<dynamic>(responseFromServer);
+            trivia_canswer.Text = obj["number"].ToString();
+            trivia_question.Text = obj["text"];
+        }
+
+        protected void trivia_submit_Click(object sender, EventArgs e)
+        {
+            if (trivia_answer.Text == trivia_canswer.Text)
+                trivia_checkanswer.Text = "You answer is correct!";
+            else
+            {
+                trivia_checkanswer.Text = "Oops! You answer is incorrect :0(  Correct answer is: " + trivia_canswer.Text;
+                trivia_submit.Enabled = false;
+            }
+        }
+
+        protected void sentiment_submit_Click(object sender, EventArgs e)
+        {
+            var postData = "txt=" + sentiment_text.Text.ToString();
+            postData += "&thing2=world";
+            var data = Encoding.ASCII.GetBytes(postData);
+            HttpWebRequest serviceRequest = (HttpWebRequest)WebRequest.Create("https://community-sentiment.p.mashape.com/text/");
+            serviceRequest.Method = "POST";
+            serviceRequest.Headers.Add("X-Mashape-Key", "WpWNTQDtlSmshAjiVtLymV1curwkp12GEdTjsn6XcyIMBQT6g6");
+            serviceRequest.ContentType = "application/x-www-form-urlencoded";
+            serviceRequest.Accept = "application/json";
+            using (var stream = serviceRequest.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            HttpWebResponse serviceResponse = (HttpWebResponse)serviceRequest.GetResponse();
+            Stream receiveStream = serviceResponse.GetResponseStream();
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader readStream = new StreamReader(receiveStream, encode, true);
+            string responseFromServer = readStream.ReadToEnd();
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            var obj = js.Deserialize<dynamic>(responseFromServer);
+            sentiment_answer.Text = "Confidence level: " +
+                                 obj["result"]["confidence"].ToString() +
+                                 " and sentiment is " +
+                                 obj["result"]["sentiment"].ToString();
+        }
+
+        protected void convert_submit_Click(object sender, EventArgs e)
+        {
+            HttpWebRequest serviceRequest = (HttpWebRequest)WebRequest.Create("https://currency-exchange.p.mashape.com/exchange?from=" + convert_from.Text.Trim() + "&q=1.0&to=" + convert_to.Text.Trim());
+            serviceRequest.Method = "GET";
+            serviceRequest.Headers.Add("X-Mashape-Key", "WpWNTQDtlSmshAjiVtLymV1curwkp12GEdTjsn6XcyIMBQT6g6");
+            serviceRequest.Accept = "text/plain";
+            HttpWebResponse serviceResponse = (HttpWebResponse)serviceRequest.GetResponse();
+            Stream receiveStream = serviceResponse.GetResponseStream();
+            Encoding encode = System.Text.Encoding.GetEncoding("utf-8");
+            StreamReader readStream = new StreamReader(receiveStream, encode, true);
+            var responseFromServer = readStream.ReadToEnd();
+            convert_rate.Text = responseFromServer;
+        }
+    }
+
+    }
